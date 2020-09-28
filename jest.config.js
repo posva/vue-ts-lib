d85757e0cf7904db319b22e659ad2cd1743d2f2f
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-28 17:39:49 +0200

    test: add missing globals

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testMatch: ['<rootDir>/__tests__/**/*.spec.ts'],
   globals: {
+    __DEV__: true,
+    __BROWSER__: true,
     'ts-jest': {
       diagnostics: {
         warnOnly: true,
