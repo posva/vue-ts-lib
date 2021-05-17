9280948463ac558d3a52bd37276f9e10f4fe3f57
Eduardo San Martin Morote <posva13@gmail.com>
2021-05-17 10:31:21 +0200

    chore: use faster jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  collectCoverage: true,
+  // only apply on test with --coverage
+  collectCoverage: false,
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['html', 'lcov', 'text'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testMatch: ['<rootDir>/__tests__/**/*.spec.ts'],
+  transform: {
+    '^.+\\.tsx?$': '@sucrase/jest-plugin',
+  },
   globals: {
     __DEV__: true,
     __BROWSER__: true,
-    'ts-jest': {
-      diagnostics: {
-        warnOnly: true,
-      },
-    },
+    __TEST__: true,
   },
   testURL: 'http://localhost/',
 }
